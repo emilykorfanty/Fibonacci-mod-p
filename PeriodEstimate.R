9937a0da2243dde520e94a53cbe12b459dc8255e
Emily Rose Korfanty <52767963+emilykorfanty@users.noreply.github.com>
2022-02-10 21:03:48 -0700

    Updated Pisano plot

--- PeriodEstimate.R
+++ PeriodEstimate.R
@@ -1,26 +1,16 @@
 # Estimate the Pisano periods for primes up to N ----
 
-get_est <- function(N, est_view=T){
-  
-  est_cds <- c("a", 
-               "b", 
-               "c", 
-               "d", 
-               "e")
-  
-  est_nms <- c("2x + 1", 
-               "x - 1", 
-               "2*(x+1)/3", 
-               "(x-1)/2", 
-               "(x+1)/3")
+get_est <- function(est_view=T){
   
   est_funs <- list(
-    a = function(x) 2*x + 1,    
-    b = function(x) x - 1,      
+    a = function(x) 2*x+1,    
+    b = function(x) x-1,      
     c = function(x) 2*(x+1)/3,  
     d = function(x) (x-1)/2,    
     e = function(x) (x+1)/3     
   )
+  
+  est_nms <- lapply(est_funs, function(X) stringr::str_replace_all(deparse(X)[2], " ", ""))
   
   # display summary of available estimates
   if(est_view){
@@ -29,7 +19,7 @@ get_est <- function(N, est_view=T){
     
     mapply(
       function(x,y) cat(paste("Enter", x, "for", y, "\n")),
-      x = est_cds,
+      x = names(est_funs),
       y = est_nms)
     
     cat("Enter 0 to include all estimates\n")
@@ -39,7 +29,7 @@ get_est <- function(N, est_view=T){
   
   est <- stringr::str_to_lower(readline("Please enter a period estimate: "))
   
-  if(est == "0") est_funs else est_funs[est]
+  if(est == "0") list(funs=est_funs, nms=est_nms) else list(funs=est_funs[est], nms=est_nms[est])
 
 }
 
